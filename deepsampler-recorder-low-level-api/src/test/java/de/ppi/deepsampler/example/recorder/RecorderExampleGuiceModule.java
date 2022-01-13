facf87ef552057c12780d811bd3057862cb5fd08
JanSchankin <jan.schankin@treeno.de>
2022-01-13 15:37:47 +0100

    🧽 Results from review

--- deepsampler-recorder-low-level-api/src/test/java/de/ppi/deepsampler/example/recorder/RecorderExampleGuiceModule.java
+++ deepsampler-recorder-low-level-api/src/test/java/de/ppi/deepsampler/example/recorder/RecorderExampleGuiceModule.java
@@ -1,7 +1,7 @@
 /*
  *
- *  * Copyright 2020 PPI AG (Hamburg, Germany)
- *  * This program is made available under the terms of the MIT License.
+ * Copyright 2020 PPI AG (Hamburg, Germany)
+ * This program is made available under the terms of the MIT License.
  *
  */
 
